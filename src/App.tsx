ea567d5fcfb2ac2baedf420cc81a9de1b3f0cbd8
sundar <sundar@mothercode.com>
2024-08-27 14:51:50 +0530

    passing functions via props - learning to reuse the components

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "Las Vegas", "Los Angeles", "London"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
