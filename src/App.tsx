fbefdbdf24798646c140c8ca90f1ebace28d6c1a
sundar <sundar@mothercode.com>
2024-08-27 18:12:40 +0530

    alert and button close added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
-      <Button color="" onClick={() => console.log("clicked")}>
-        Hello World
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisible(true)}>
+        click me
       </Button>
     </div>
   );
